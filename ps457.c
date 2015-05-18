c0e426b084634be266cdfddbc9a50fe5ad8607f1
Chris Wozniak <cwoz117@hotmail.ca>
2015-05-18 14:55:54 -0600

    layout organized

--- ps457.c
+++ ps457.c
@@ -7,13 +7,42 @@ Author: Chris Wozniak
 linux enviruments, with switches that expand on
 additional PID info.
  * * * * * * * * * * * * * * * * * * * * * * * * */
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
-  // If there are no args, do the default
-
-  // else check the switches.
-
-
+  /* SWITCH CONFIG*/
+  // -p <PID> outputs only the requested PID
+  // -s look in the STAT dir for single-char state info
+  // -U user time consumed (STAT 14th field)
+  // -S system time consumed (STAT 15th field) DEFAULT
+  // -v virtual memory used (STATM 1st field) DEFAULT
+  // -c parent process (cmdline, could be null) DEFAULT
   
   return 0;
 }
+
+char[] pidInfo{
+
+  return 'hello';
+}
+char[] singleCharState(char[] pid){
+
+  return 'hello';
+}
+char[] userTimeCons(char[] pid){
+
+  return 'hello';
+}
+char[] sysTimeCons(char[] pid){
+
+  return 'hello';
+}
+char[] virMemUsed(char[] pid){
+
+  return 'hello';
+}
+char[] parentProc(char[] pid){
+
+  return 'hello';
+}
+
