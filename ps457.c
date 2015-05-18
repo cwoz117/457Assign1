e06f69331653a3d0635725fce4b8c643f56d6749
Chris Wozniak <cwoz117@hotmail.ca>
2015-05-18 01:50:38 -0600

    started the frame

--- ps457.c
+++ ps457.c
@@ -1,4 +1,19 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * *
+                      ps457
+Author: Chris Wozniak
+    ID: 10109820
+
+    ps457.c is an extension of the ps command in
+linux enviruments, with switches that expand on
+additional PID info.
+ * * * * * * * * * * * * * * * * * * * * * * * * */
+
 int main(int argc, char *argv[]){
-  printf("Hello there\n");
+  // If there are no args, do the default
+
+  // else check the switches.
+
+
+  
   return 0;
 }
