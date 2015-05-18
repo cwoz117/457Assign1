7eec03d2a908e611e3022dbe0b1e443466a83d1d
Chris Wozniak <cwoz117@hotmail.ca>
2015-05-18 16:46:48 -0600

    bugged findSwitch

--- ps457.c
+++ ps457.c
@@ -8,8 +8,33 @@ linux enviruments, with switches that expand on
 additional PID info.
  * * * * * * * * * * * * * * * * * * * * * * * * */
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+//Switch lookup takes the arguments, and searches them for
+//a provided switch. As they can be unordered, we must
+//check every location except position 0, which contains
+//the function name itself.
+int findSwitch(int argLength, char *argp[], char flag){
+  int i = 1;
+  int found = 0;
+  while (i < argLength){
+    char chArg[strlen(argp[i])];
+    strcpy(chArg, argp[i]);
+    if (chArg[0] == '-'){
+      if (chArg[1] == flag)
+	found  = 1;
+    }
+    i++;
+  }
+  return found;
+}
 
 int main(int argc, char *argv[]){
+  printf("hello \n");
+  int c = findSwitch(argc, &argv, 'c');
+  if (c == 1)
+      printf("found c");
   /* SWITCH CONFIG*/
   // -p <PID> outputs only the requested PID
   // -s look in the STAT dir for single-char state info
@@ -20,29 +45,29 @@ int main(int argc, char *argv[]){
   
   return 0;
 }
-
-char[] pidInfo{
-
-  return 'hello';
-}
-char[] singleCharState(char[] pid){
+/*
+&char[] pidInfo(){
 
   return 'hello';
 }
-char[] userTimeCons(char[] pid){
+&char[] singleCharState(char[] pid){
 
   return 'hello';
 }
-char[] sysTimeCons(char[] pid){
+&char[] userTimeCons(char[] pid){
 
   return 'hello';
 }
-char[] virMemUsed(char[] pid){
+&char[] sysTimeCons(char[] pid){
 
   return 'hello';
 }
-char[] parentProc(char[] pid){
+&char[] virMemUsed(char[] pid){
 
   return 'hello';
 }
+&char[] parentProc(char[] pid){
 
+  return 'hello';
+}
+*/
