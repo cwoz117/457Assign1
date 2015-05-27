011a6d952889e3b9280cbb439e6681b1c3840c3f
cwoz117 <cwoz117@hotmail.ca>
2015-05-26 20:52:12 -0600

    testing phase

--- ps457.c
+++ ps457.c
@@ -22,7 +22,7 @@
 *              and prints them to the screen in an ordered                     *
 *              stream.                                                         *
 *                                                                              *
-* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 void printOut(FILE *fd, char *vals[]){
       int i = 0;
       for (i = 0; i < 6; i++){
@@ -50,7 +50,7 @@ printHead(FILE *fd, int mask){
 
 
 }
-
+*/
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *                                                                              *
 *                                  main                                        *
@@ -61,57 +61,26 @@ printHead(FILE *fd, int mask){
 *                                                                              *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 int main(int argc, char *argv[]){
-      int swMask = 0;   // bitmask where each bit represents a switch
-      int p;            // points to where the pid is in argv.
-      FILE *output;     // holds all output data.
-      char buffer[1024];
-      char *pid;
 
-      output = fopen (output, "w");
-      if (output == NULL){
-            printf("Error opening output file!");
-            exit(1);
-      }
+      int swMsk = 0;
 
-      swMask = findSwitch(argc, argv);
-      printHead(output, swMask);
-      if (swMask & 32){
-            pid = findPID(argc, argv);
-            fprintf(output, pid);
-            if (swMask & 16)
-                   fprintf(output, getData(buildPath("/proc", argv[pid], "stat"), 3));
-            if (swMask & 8)
-                  fprintf(output, getData(buildPath("/proc", argv[pid], "stat"), 14));
-            if (swMask & 4)
-                  fprintf(output, getData(buildPath("/proc", argv[pid], "stat"), 15));
-            if (swMask & 2)
-                  fprintf(output, getData(buildPath("/proc", argv[pid], "statm"), 1));
-            if (swMask & 1)
-                  fprintf(output, getData(buildPath("/proc", argv[pid], "stat"), 1));
-            fprintf(output, "\n");
+      swMsk = findSwitch(argc, argv);
+      printf("%d\n", swMsk);
 
-            while (fgets(buffer, 1024, output) != NULL) {
-                  printf("%s", buffer);
-            }
-      } else {
-            /*char *pidList;
-            int i = 1;
-            while ((pidList = getData("ls /proc"), i) != NULL) {
-                  output[0] = pidList;
-                  if (swMask & 16)
-                        output[1] = getData(buildPath("/proc", pidList, "stat"), 3);
-                  if (swMask & 8)
-                        output[2] = getData(buildPath("/proc", pidList, "stat"), 14);
-                  if (swMask & 4)
-                        output[3] = getData(buildPath("/proc", pidList, "stat"), 15);
-                  if (swMask & 2)
-                        output[4] = getData(buildPath("/proc", pidList, "statm"), 1);
-                  if (swMask & 1)
-                        output[5] = getData(buildPath("/proc", pidList, "stat"), 1);
-                  printOut(output);
-                  i++;
-            }*/
 
-      }
+
+
+
+
+
+
+
+
+
+
+
+
+
       return 0;
+
 }
